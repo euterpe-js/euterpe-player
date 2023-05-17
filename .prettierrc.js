55d46e8753703ad177eb75982b3ffeceee4251cb
Djkáťo <radovan.katrencik@gmail.com>
2023-05-18 00:08:09 +0200

    Create .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    parser: "typescript",
+    trailingComma: "none",
+    useTabs: true,
+    tabWidth: 4,
+    semi: false,
+    singleQuote: false,
+    endOfLine: "lf",
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: "always"
+}
